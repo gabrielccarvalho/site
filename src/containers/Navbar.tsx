cd269833d19827eb45bc7d3f0fdb6687e56d8b6c
gabrielccarvalho <caampos22@gmail.com>
2020-07-17 21:40:16 -0300

    feat: Empty navbar

---
+++ src/containers/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Container } from 'styled-bootstrap-grid'
+
+const Wrapper = styled(Container)``
+
+const Navbar = () => {
+  return (
+    <>
+      <Wrapper />
+    </>
+  )
+}
+
+export default Navbar
